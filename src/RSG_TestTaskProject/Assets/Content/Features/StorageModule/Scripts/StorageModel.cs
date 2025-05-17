a2c26be61e168d5a70d64b3dd65bcdbe8fbcc5e3
Olsney <104034829+Olsney@users.noreply.github.com>
2025-05-17 15:17:19 +0300

    Changed storage using logic

---
+++ src/RSG_TestTaskProject/Assets/Content/Features/StorageModule/Scripts/StorageModel.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace Content.Features.StorageModule.Scripts
+{
+    public class StorageModel
+    {
+        private readonly List<Item> _items = new List<Item>();
+        private readonly float _maxWeight;
+
+        private float _currentWeight;
+
+        public StorageModel(float maxWeight)
+        {
+            _maxWeight = maxWeight;
+            _currentWeight = 0f;
+        }
+
+        public float CurrentWeight { get; private set; }
+        
+        public float MaxWeight => _maxWeight;
+        public List<Item> Items => new List<Item>(_items);
+    }
+}
