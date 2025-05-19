b050fb4dfe3959c127942c2689fbb6d677d6a2da
Olsney <104034829+Olsney@users.noreply.github.com>
2025-05-19 13:13:50 +0300

    Added heal view and item type in item

--- src/RSG_TestTaskProject/Assets/Content/Features/StorageModule/Scripts/Item.cs
+++ src/RSG_TestTaskProject/Assets/Content/Features/StorageModule/Scripts/Item.cs
@@ -7,6 +7,7 @@ namespace Content.Features.StorageModule.Scripts {
         public Sprite Icon { get; private set; }
         public int Price { get; private set; }
         public float Weight { get; private set; }
+        public ItemType ItemType { get; private set; }
 
         public Item(string name, Sprite icon, int price, float weight) {
             Name = name;
@@ -20,6 +21,7 @@ namespace Content.Features.StorageModule.Scripts {
             Name = itemConfiguration.Name;
             Icon = itemConfiguration.Icon;
             Price = itemConfiguration.Price;
+            ItemType = itemConfiguration.ItemType;
         }
     }
 }
